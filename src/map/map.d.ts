84b76846ec57de5b624fef39cc39db8ab2aabb5b
komed3 <pk97@outlook.de>
2025-09-30 01:15:01 +0200

    add map unit types

--- src/map/map.d.ts
+++ src/map/map.d.ts
@@ -11,6 +11,22 @@ export type APMapUnits = 'default' | 'metric' | 'imperial';
 
 export type APMapUnitSystems = 'metric' | 'imperial' | 'avionic';
 
+export type APMapUnitSystemType = 'distance' | 'speed' | 'altitude';
+
+type UnitConversionMap< S extends APMapUnitSystems > = {
+    [ K in Exclude< APMapUnitSystems, S > ]: number;
+};
+
+export interface APMapUnitSystemEntry< S extends APMapUnitSystems > {
+    base: S;
+    convert: UnitConversionMap< S >;
+    units: Record< string, number >;
+}
+
+export type APMapUnitSystem = {
+    [ T in APMapUnitSystemType ]: APMapUnitSystemEntry< APMapUnitSystems >;
+};
+
 export type APMapPerformance = 'high' | 'medium' | 'low';
 
 export type APMapEventType =
