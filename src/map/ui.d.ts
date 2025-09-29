28935942794bd724283f7f25c3598b2636259ee1
komed3 <pk97@outlook.de>
2025-09-29 10:14:51 +0200

    Update map/ui.d.ts

--- src/map/ui.d.ts
+++ src/map/ui.d.ts
@@ -1,10 +1,8 @@
-export type APMapUIEnabled = boolean | 'auto';
-
 export type APMapUIControlGroup =
     'attributionControl' | 'fullscreenControl' | 'headingControl' |
     'layerControl' | 'settingsControl' | 'stateControl' | 'theaterControl' |
     'trackingControl' | 'zoomControl';
 
 export type APMapUIControl = Partial< Record< APMapUIControlGroup, {
-    enabled?: APMapUIEnabled;
+    enabled?: boolean;
 } > >;
