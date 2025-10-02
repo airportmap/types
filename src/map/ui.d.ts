e84af0acec1244862675d77f40c5c53c4ba0ee96
komed3 <pk97@outlook.de>
2025-10-02 23:29:09 +0200

    add APMapUIBntOptions interface

--- src/map/ui.d.ts
+++ src/map/ui.d.ts
@@ -6,3 +6,11 @@ export type APMapUIControlGroup =
 export type APMapUIControl = Partial< Record< APMapUIControlGroup, {
     enabled?: boolean;
 } > >;
+
+export interface APMapUIBntOptions {
+    handler?: CallableFunction;
+    icon?: string;
+    activeIcon?: string;
+    ariaLabel?: string;
+    classes?: string;
+}
